f16b059a766a67593ce24469721848c4fb262eb0
sandi prayoga <mrsandip1106@gmail.com>
2026-01-28 07:23:44 +0700

    pengaduan

--- app/Models/Pengaduan.php
+++ app/Models/Pengaduan.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pengaduan extends Model
 {
-    //
+    use HasFactory;
+
+    // 🔥 PENTING: Daftarkan semua kolom yang boleh diisi lewat Form
+    protected $fillable = [
+        'nama_pelapor',
+        'nik',
+        'no_hp',
+        'judul_laporan',
+        'isi_laporan',
+        'foto_bukti',
+        'status',
+        'tanggapan_desa'
+    ];
 }
